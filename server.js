e6c15f09ac640d73b7885650d0005afa4f2dfd72
Garrett Peterson <GarrettPeterson@smarttraining.com>
2026-09-03 09:13:18 -0500

    Week1

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -6,6 +8,11 @@ const port = process.env.PORT || 3000;
 app.use('/', require('./routes'));
 
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+mongodb.initDB((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {console.log(`Databse is listening and node is running on port ${port}`)});
+    }
 });
