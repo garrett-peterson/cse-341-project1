4dec584279bcee77e8e98d70650402bcd7e9e0b7
Garrett Peterson <GarrettPeterson@smarttraining.com>
2026-09-09 09:37:53 -0500

    Added a PUT, POST and DELETE

---
+++ controllers/contacts.js
@@ -0,0 +1,74 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    });
+};
+
+const getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users[0]);
+    });
+};
+
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user);
+    if (response.acknowledged > 0) {
+        res.status(204).send();
+    }
+    else {
+        res.status(400).json(response.error || 'Some error occured while creating the contact');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId}, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    }
+    else {
+        res.status(400).json(response.error || 'Some error occured while updating the contact');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    }
+    else {
+        res.status(400).json(response.error || 'Some error occured while deleting the contact');
+    }
+};
+
+module.exports = {
+    getAll,
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
+};
